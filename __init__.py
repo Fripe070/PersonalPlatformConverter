c094dc189ce8be4c3f0f7198801a913d60fe8c8b
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-05-29 19:21:17 +0200

    General cleanup and make slash command into a hybrid command

--- __init__.py
+++ __init__.py
@@ -3,14 +3,14 @@ import re
 
 import aiohttp
 import discord
-from discord import app_commands
+from discord.ext import commands
 # noinspection PyFromFutureImport
 from youtubesearchpython.__future__ import VideosSearch
 
 import breadcord
 
 
-def extract_ids_from_url(url: str, /) -> list[str]:
+def extract_ids_from_urls(url: str, /) -> list[str]:
     return re.findall(
         r"https?://open\.spotify\.com/track/(\w+)",
         url,
@@ -45,7 +45,7 @@ class NoSpotify(breadcord.module.ModuleCog):
                 }
             ) as response:
                 data = await response.json()
-                if data["error"] == "invalid_client":
+                if data.get("error") == "invalid_client":
                     raise ValueError("Invalid spotify client id or secret")
                 self.settings.api_key.value = data["access_token"]
 
@@ -61,26 +61,32 @@ class NoSpotify(breadcord.module.ModuleCog):
                 raise ValueError("Invalid spotify token")
             return await response.json()
 
-    async def spotify_to_youtube(self, url: str, /) -> str:
-        track_data = await self.get_spotify_track(url)
+    async def spotify_id_to_youtube(self, track_id: str, /) -> str:
+        track_data = await self.get_spotify_track(track_id)
         track_artists = [artist["name"] for artist in track_data["artists"]]
         track_name = track_data["name"]
 
         video = (await VideosSearch(f"{track_name} {' '.join(track_artists)}", limit=1).next())["result"][0]
         return video["link"]
 
-    @app_commands.command(description="Get youtube video from a spotify track url")
-    async def spotify_to_yt(self, interaction: discord.Interaction, url: str) -> None:
-        urls = extract_ids_from_url(url)
-        if not urls:
-            await interaction.response.send_message("Could not find track ID in the specified URL.", ephemeral=True)
-        await interaction.response.send_message(await self.spotify_to_youtube(urls[0]), ephemeral=True)
+    async def spotify_url_to_youtube(self, url: str, /) -> str:
+        if ids := extract_ids_from_urls(url):
+            return await self.spotify_id_to_youtube(ids[0])
+        else:
+            raise ValueError("Could not find a track ID in the specified URL.")
+
+    @commands.hybrid_command(description="Get youtube video from a spotify track url")
+    async def spotify_to_yt(self, ctx: commands.Context, url: str) -> None:
+        try:
+            await ctx.reply(await self.spotify_url_to_youtube(url), ephemeral=True)
+        except ValueError as error:
+            await ctx.reply(str(error), ephemeral=True)
 
     @breadcord.module.ModuleCog.listener()
     async def on_message(self, message: discord.Message) -> None:
         if not self.settings.search_messages.value:
             return
-        if urls := [await self.spotify_to_youtube(track_id) for track_id in extract_ids_from_url(message.content)]:
+        if urls := [await self.spotify_id_to_youtube(track_id) for track_id in extract_ids_from_urls(message.content)]:
             await message.reply(" ".join(urls), mention_author=False)
 
 
