6c3f96547fc6ae9438826a824ead28f4b1768b81
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-08-11 15:28:18 +0200

    Update YouTube Music API for new system

--- api/platforms/youtube_music.py
+++ api/platforms/youtube_music.py
@@ -6,17 +6,17 @@ from ..errors import InvalidURLError
 
 
 class YoutubeMusicAPI(YoutubeAPI):
-    def extract_track_id(self, video_url: str, /) -> str:
+    def get_track_id(self, track_url: str) -> str:
         if matches := re.match(
             r"^(?:https?://)?music\.youtube\.com/watch\?v=([a-zA-Z0-9_\-]+)",
-            video_url,
+            track_url,
             flags=re.ASCII,
         ):
             return matches[0]
         else:
             raise InvalidURLError("Invalid Youtube Music url")
 
-    async def search_tracks(self, query: str, /) -> list[UniversalTrack] | None:
+    async def search_tracks(self, query: str) -> list[UniversalTrack] | None:
         tracks = await super().search_tracks(query)
         for track in tracks:
             track.url = re.sub(r"^https?://(www\.)?(youtu\.be|youtube.[a-z]+)", "^https://music.youtube.com", track.url)
