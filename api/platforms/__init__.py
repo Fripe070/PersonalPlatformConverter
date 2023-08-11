2699b890cbb6c9bc52d37600115285a987a1dbe9
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-08-11 15:30:22 +0200

    Update Beatsaver API for new system

--- api/platforms/__init__.py
+++ api/platforms/__init__.py
@@ -1,3 +1,4 @@
+from .beatsaver import BeatSaverAPI
 from .spotify import SpotifyAPI
 from .youtube import YoutubeAPI
 from .youtube_music import YoutubeMusicAPI
--- api/platforms/beatsaver.py
+++ api/platforms/beatsaver.py
@@ -5,34 +5,32 @@ from ..abc import AbstractAPI, UniversalTrack
 from ..errors import InvalidURLError
 
 
+def beatsaver_map_to_universal(custom_map: dict) -> UniversalTrack:
+    return UniversalTrack(
+        title=custom_map["metadata"]["songName"],
+        artist_names=[custom_map["metadata"]["songAuthorName"]],
+        url=f"https://beatsaver.com/maps/{custom_map['id']}",
+        cover_url=custom_map["versions"][0]["coverURL"],
+    )
+
 class BeatSaverAPI(AbstractAPI):
     api_base = "https://api.beatsaver.com"
 
-    def extract_track_id(self, video_url: str, /) -> str:
+    def get_track_id(self, video_url: str, /) -> str:
         if matches := re.match(r"^(?:https?://)?beatsaver.com/maps([a-z0-9]+)", video_url):
             return matches[0]
         else:
             raise InvalidURLError("Invalid beatsaver map url")
 
-    async def url_to_query(self, map_url: str, /) -> str:
-        async with self.session.get(f"{self.api_base}/maps/id/{self.extract_track_id(map_url)}") as response:
+    async def track_from_id(self, track_id: str) -> UniversalTrack | None:
+        async with self.session.get(f"{self.api_base}/maps/id/{track_id}") as response:
             map_metadata = (await response.json())["metadata"]
+            return beatsaver_map_to_universal(map_metadata)
 
-        return f"{map_metadata['songName']} {map_metadata['songAuthorName']}"
-
-    async def search_tracks(self, query: str, /) -> list[UniversalTrack] | None:
+    async def search_tracks(self, query: str) -> list[UniversalTrack] | None:
         async with self.session.get(
             f"{self.api_base}/search/text/0?sortOrder=Rating&q={urllib.parse.quote(query)}",
         ) as response:
             maps = (await response.json())["docs"]
-
-        return [
-            UniversalTrack(
-                title=custom_map["metadata"]["songName"],
-                artist_names=[custom_map["metadata"]["songAuthorName"]],
-                url=f"https://beatsaver.com/maps/{custom_map['id']}",
-                cover_url=custom_map["versions"][0]["coverURL"],
-            )
-            for custom_map in maps
-        ]
+            return [beatsaver_map_to_universal(custom_map) for custom_map in maps]
 
